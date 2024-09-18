4d19a468ff0827dad608198ee3a2ebe79f6ff8f9
Priler <priler96@gmail.com>
2024-09-18 18:11:21 +0500

    AI anti-spam model implementation

--- models/member.py
+++ models/member.py
@@ -5,5 +5,5 @@ class Member(ormar.Model):
     ormar_config = ormar_config.copy(tablename="members")
 
     id: int = ormar.Integer(primary_key=True, auto_increment=True)
-    user_id: int = ormar.Integer()
+    user_id: int = ormar.BigInteger(unique=True)
     messages_count: int = ormar.Integer()
