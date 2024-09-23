5c0fa3b4befc90d4021500ab24d76e3b86c0d0a4
Priler <priler96@gmail.com>
2024-09-23 16:23:31 +0500

    Improved antispam AI
    Added actions keyboard to react on spam messages, you can mark em spam/not a spam and they being saved to DB for further AI model improvements

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -6,7 +6,10 @@ import localization
 
 from contextlib import suppress
 from aiogram.utils.exceptions import (MessageToEditNotFound, MessageCantBeEdited, MessageCantBeDeleted,
-                                      MessageToDeleteNotFound)
+                                      MessageToDeleteNotFound, CantRestrictChatOwner)
+
+from models.member import Member
+from models.spam import Spam
 
 @dp.callback_query_handler()
 async def callback_handler(call: types.CallbackQuery):
@@ -15,6 +18,10 @@ async def callback_handler(call: types.CallbackQuery):
 
     :param call: Callback with action put into call.data field
     """
+
+    ###
+    ### REPORT callbacks
+    ###
     if call.data.startswith("del_"):
         with suppress(MessageCantBeDeleted, MessageToDeleteNotFound):
             await call.message.bot.delete_message(config.groups.main, int(call.data.split("_")[1]))
@@ -103,3 +110,37 @@ async def callback_handler(call: types.CallbackQuery):
                                                  text=call.message.text + localization.get_string(
                                                      "action_deleted_dismissed4"))
         await call.answer(text="Done")
+
+    ###
+    ### SPAM callbacks
+    ###
+    elif call.data.startswith("spam_test_"):
+        # delete record
+        await Spam.objects.delete(id=int(call.data.split("_")[2]))
+
+        # increase member messages count, cuz is not a spam :3
+        member = Member.objects.get(id=int(call.data.split("_")[3]))
+        member.messages_count += 1
+        await member.update()
+
+        await call.message.bot.edit_message_text(chat_id=config.groups.logs,
+                                                 message_id=call.message.message_id,
+                                                 text=call.message.text + "\n\n<b>Удалено из базы, вероятно тест.</b>")
+        await call.answer(text="Done")
+    elif call.data.startswith("spam_ban_"):
+        with suppress(CantRestrictChatOwner):
+            await call.message.bot.kick_chat_member(chat_id=config.groups.main, user_id=call.data.split("_")[2])
+
+        await call.message.bot.edit_message_text(chat_id=config.groups.logs,
+                                                 message_id=call.message.message_id,
+                                                 text=call.message.text + "\n\n❌ <b>Юзер забанен, сообщение помечено как спам</b>")
+        await call.answer(text="Done")
+    elif call.data.startswith("spam_invert_"):
+        # retrieve record
+        spam_rec = await Spam.objects.get(id=int(call.data.split("_")[2]))
+        spam_rec.is_spam = False
+        await spam_rec.update()
+
+        await call.message.bot.edit_message_text(chat_id=config.groups.logs,
+                                                 message_id=call.message.message_id,
+                                                 text=call.message.text + "\n\n❎ <b>Сообщение помечено как НЕ СПАМ</b>")
---
+++ models/spam.py
@@ -0,0 +1,9 @@
+import ormar
+from db import ormar_config
+
+class Spam(ormar.Model):
+    ormar_config = ormar_config.copy(tablename="spam")
+
+    id: int = ormar.Integer(primary_key=True, auto_increment=True)
+    message: str = ormar.Text(unique=True)
+    is_spam: bool = ormar.Boolean()
