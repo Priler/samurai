e737fd26a78e1dfcdc051b20c196a26de55a186d
Priler <priler96@gmail.com>
2024-10-16 22:05:21 +0500

    setlvl disabled for admins

--- filters.py
+++ filters.py
@@ -1,6 +1,21 @@
 from aiogram import types
 from aiogram.dispatcher.filters import BoundFilter
 
+from configurator import config
+
+
+class IsOwnerFilter(BoundFilter):
+    """
+    Custom filter "is_owner".
+    """
+    key = "is_owner"
+
+    def __init__(self, is_owner):
+        self.is_owner = is_owner
+
+    async def check(self, message: types.Message):
+        return message.from_user.id == config.bot.owner
+
 
 class IsAdminFilter(BoundFilter):
     """
