5ac930e527f29990d9d9373f52a4efd58a9552c4
Priler <priler96@gmail.com>
2025-10-05 18:33:36 +0500

    spam substrings check added

--- ruspam.py
+++ ruspam.py
@@ -5,7 +5,14 @@ model_path = "ruspam_model/"
 tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
 model = AutoModelForSequenceClassification.from_pretrained(model_path, local_files_only=True)
 
+spam_substrings = [
+    "official_vpnbot",
+]
+
 def predict(text):
+    if any(sub.lower() in text.lower() for sub in spam_substrings):
+        return True
+
     inputs = tokenizer(text, return_tensors="pt", truncation=True, max_length=256)
     with torch.no_grad():
         outputs = model(**inputs)
