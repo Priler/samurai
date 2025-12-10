092281349ea935125fe02e6af248b0f3f34184e1
Priler <priler96@gmail.com>
2025-12-11 03:54:37 +0500

    db_init script added back

--- db_init.py
+++ db_init.py
@@ -1,28 +1,49 @@
 """
 Database initialization script.
-Run this once to create the database tables.
+
+This script can be used to init or re-init database tables.
+CAUTION: it will DROP ALL DATA in the tables!
+
+Uncomment the exit() line below to run it.
 """
+exit("COMMENT THIS LINE IN ORDER TO RE-INIT DATABASE TABLES")
+
 import asyncio
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 from db.database import ormar_config
 from db.models import Member, Spam
 
 
-async def init_database() -> None:
-    """Initialize the database and create tables."""
-    # Connect to database
+async def reinit_db_tables() -> None:
+    """Drop and recreate all database tables."""
+    logger.info("Connecting to database...")
+    
     if not ormar_config.database.is_connected:
         await ormar_config.database.connect()
 
-    # Create tables using SQLAlchemy
+    logger.warning("Dropping all tables...")
+    async with ormar_config.engine.begin() as conn:
+        await conn.run_sync(ormar_config.metadata.drop_all)
+    
+    logger.info("Creating tables...")
     async with ormar_config.engine.begin() as conn:
         await conn.run_sync(ormar_config.metadata.create_all)
 
-    print("Database tables created successfully!")
-
-    # Disconnect
     await ormar_config.database.disconnect()
+    await ormar_config.engine.dispose()
+    
+    logger.info("Database tables recreated successfully!")
 
 
 if __name__ == "__main__":
-    asyncio.run(init_database())
+    asyncio.run(reinit_db_tables())
+    exit("DONE")
+
+
+# For SQLite you can also use synchronous version:
+# ormar_config.metadata.drop_all(ormar_config.engine)
+# ormar_config.metadata.create_all(ormar_config.engine)
